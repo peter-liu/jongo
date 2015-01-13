54d295eb8a2c71f00d791a13b5d04bd27c2c0b32
pangzi@jm <liusz0503@mgial.com>
2015-01-13 20:49:16 +0800

    extend queryfactory to support expression query

---
+++ src/main/java/org/jongo/query/ExprQueryFactory.java
@@ -0,0 +1,8 @@
+package org.jongo.query;
+
+/**
+ * TODO description
+ * Created by liusz on 15-1-13-下午2:34
+ */
+public class ExprQueryFactory {
+}
---
+++ src/main/java/org/jongo/query/QueryFactoryDecorator.java
@@ -0,0 +1,8 @@
+package org.jongo.query;
+
+/**
+ * TODO description
+ * Created by liusz on 15-1-13-下午2:35
+ */
+public class QueryFactoryDecorator {
+}
